bd5d59f2609553356b2bb59d87bc7da4b473aeb7
Abhishek Kumar <CE512@compassitesinc.com>
2018-07-18 13:25:36 +0530

    testing123..

--- src/abc/ABC1.java
+++ src/abc/ABC1.java
@@ -1,11 +1,10 @@
 package abc;
 
-public class ABC {
+public class ABC1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World..");
-		System.out.println("Modified..");
+		System.out.println("New File");
 
 	}
 
